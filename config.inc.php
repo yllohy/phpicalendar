e81bac010a723999b5a8f1dd0771ed69a4935e26
Chad Little <clittle@users.sourceforge.net>
2002-09-16 05:18:57 +0000

    Latest changes for 0.2 release.

--- config.inc.php
+++ config.inc.php
@@ -1,1 +1,1 @@
-<?if(phpversion() >= "4.2.0") 	{		extract($HTTP_POST_VARS);		extract($HTTP_GET_VARS);		}$style_sheet = "default.css";// what iCal file are we using$fullpath = "webcal://chadsdomain.com/ical/Home.ics";$filename = "calendars/Home.ics";?>
+<?// Retain some compatibility backwards like.if(phpversion() >= "4.2.0") 	{		extract($HTTP_POST_VARS);		extract($HTTP_GET_VARS);		}$style_sheet = "default.css";// what iCal file are we using$fullpath = "webcal://";$filename = "calendars/Home.ics";?>
