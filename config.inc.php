6cdc79648a1def0d0582fc6584687f529e46972b
Chad Little <clittle@users.sourceforge.net>
2002-09-21 04:51:54 +0000

    Added language support.

--- config.inc.php
+++ config.inc.php
@@ -8,17 +8,18 @@ if(phpversion() >= "4.2.0")
 		extract($HTTP_GET_VARS);	
 	}
 
-$style_sheet = "default.css";
-
-// what iCal file are we using
-$fullpath = "webcal://";
-
-// path to directory with calendars
-$calendar_path = "./calendars";
-
-// filename of calendar without .ics
+$style_sheet = "default.css";			// Themes support
+$fullpath = "webcal://";				// what iCal file are we using
+$calendar_path = "./calendars";			// path to directory with calendars
+$default_view = "day";					// filename of calendar without .ics
 $default_cal = "Home";
+$language = "English";					// Language support
 
-$default_view = "day";
+
+if ($language == "English") { 	
+	include "languages/english.inc.php";
+} else {
+	include "languages/english.inc.php";
+}
 
 ?>
