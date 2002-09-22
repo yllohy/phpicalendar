181bc144e88c8bc4ed06a4fb043e1fd20aaf09c7
Chad Little <clittle@users.sourceforge.net>
2002-09-22 23:22:00 +0000

    Initial event.php commit.

---
+++ event.php
@@ -0,0 +1,32 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+        "http://www.w3.org/TR/1999/REC-html401-19991224/loose.dtd">
+<html>
+<head>
+  <meta http-equiv="content-type" content="text/html;charset=UTF-8">
+  <title><? echo "$calendar_name"; ?></title>
+	<link rel="stylesheet" type="text/css" href="styles/<? echo "$style_sheet"; ?>">
+</head>
+ <body bgcolor="#eeeeee" leftmargin="0" marginheight="0" marginwidth="0" topmargin="0" rightmargin="0">
+   <br>
+   <table border="0" cellspacing="0" cellpadding="0">
+    <tr height="18">
+     <td nowrap width="7" height="18"></td>
+     <td align="right" valign="top" height="18"><b>Event:</b></td>
+     <td nowrap width="7" height="18"></td>
+     <td align="left" valign="top" height="18">Recurring Daily</td>
+    </tr>
+    <tr height="18">
+     <td nowrap width="7" height="18"></td>
+     <td align="right" valign="top" height="18"><b>Start Date:</b></td>
+     <td width="7" height="18"></td>
+     <td align="left" valign="top" height="18">Tuesday, September 24 10:00AM</td>
+    </tr>
+    <tr height="18">
+     <td nowrap width="7" height="18"></td>
+     <td align="right" valign="top" height="18"><b>End Date:</b></td>
+     <td width="7" height="18"></td>
+     <td align="left" valign="top" height="18">Tuesday, September 24 11:30AM</td>
+    </tr>
+   </table>
+ </body>
+</html>
