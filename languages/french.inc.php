9b6ebaaeb980024c9f932fc5c19c0d7b5f850bc7
Chad Little <clittle@users.sourceforge.net>
2002-09-22 21:49:04 +0000

    French lang include.

---
+++ languages/french.inc.php
@@ -0,0 +1,31 @@
+<?
+
+// French language include
+// For version 0.4 PHP iCalendar
+//
+// Translation by La Shampouineuse (info@lashampoo.com)
+//
+// Submit new translations to chad@chadsdomain.com
+
+
+
+$day_lang			= "Jour";
+$week_lang			= "Semaine";
+$month_lang			= "Mois";
+$year_lang			= "Ann&eacute;e";
+$calendar_lang		= "Calendrier";
+$next_day_lang		= "Jour suivant";
+$next_month_lang	= "Mois suivant";
+$next_week_lang		= "Semaine suivante";
+$next_year_lang		= "Ann&eacute;e suivante";
+$last_day_lang		= "Jour pr&eacute;c&eacute;dent";
+$last_month_lang	= "Mois pr&eacute;c&eacute;dent";
+$last_week_lang		= "Semaine pr&eacute;c&eacute;dente";
+$last_year_lang		= "Ann&eacute;e pr&eacute;c&eacute;dente";
+$subscribe_lang		= "Souscrire";
+$download_lang		= "T&eacute;l&eacute;charger";
+$powered_by_lang 	= "Produit avec";
+$version_lang		= "0.4";
+
+
+?>
