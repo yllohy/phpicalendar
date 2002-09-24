82003a697eb5bfed137ba02e60e6a4266329b42e
jwangen <jwangen>
2002-09-24 01:08:15 +0000

    converted mac line endings to unix

--- languages/japanese.inc.php
+++ languages/japanese.inc.php
@@ -1,1 +1,60 @@
-<?// English language include// For version 0.4 PHP iCalendar//// Translation by Jared Wangen (jared@silter.org)//// Submit new translations to chad@chadsdomain.com$day_lang		= "日";$week_lang		= "週";$month_lang		= "月";$year_lang		= "年";$calendar_lang		= "カレンダー";$next_day_lang		= "翌日";$next_month_lang	= "来月";$next_week_lang		= "来週";$next_year_lang		= "明年";$last_day_lang		= "前日";$last_month_lang	= "先月";$last_week_lang		= "先週";$last_year_lang		= "去年";$subscribe_lang		= "追加";$download_lang		= "ダウンロド";$powered_by_lang 	= "Powered by";$version_lang		= "0.4";$month_jan_lang		= "1月";$month_feb_lang		= "2月";$month_mar_lang		= "3月";$month_apr_lang		= "4月";$month_may_lang		= "5月";$month_jun_lang		= "6月";$month_jul_lang		= "7月";$month_aug_lang		= "8月";$month_sep_lang		= "9月";$month_oct_lang		= "10月";$month_nov_lang		= "11月";$month_dec_lang		= "12月";$day_sun_short_lang	= "日曜";$day_mon_short_lang	= "月曜";		$day_tue_short_lang	= "火曜";$day_wed_short_lang	= "水曜";$day_thu_short_lang	= "木曜";$day_fri_short_lang	= "金曜";$day_sat_short_lang	= "土曜";$day_sun_lang		= "日曜日";$day_mon_lang		= "月曜日";		$day_tue_lang		= "火曜日";$day_wed_lang		= "水曜日";$day_thu_lang		= "木曜日";$day_fri_lang		= "金曜日";$day_sat_lang		= "土曜日";?>
+<?
+
+// English language include
+// For version 0.4 PHP iCalendar
+//
+// Translation by Jared Wangen (jared@silter.org)
+//
+// Submit new translations to chad@chadsdomain.com
+
+
+
+$day_lang		= "日";
+$week_lang		= "週";
+$month_lang		= "月";
+$year_lang		= "年";
+$calendar_lang		= "カレンダー";
+$next_day_lang		= "翌日";
+$next_month_lang	= "来月";
+$next_week_lang		= "来週";
+$next_year_lang		= "明年";
+$last_day_lang		= "前日";
+$last_month_lang	= "先月";
+$last_week_lang		= "先週";
+$last_year_lang		= "去年";
+$subscribe_lang		= "追加";
+$download_lang		= "ダウンロド";
+$powered_by_lang 	= "Powered by";
+$version_lang		= "0.4";
+
+$month_jan_lang		= "1月";
+$month_feb_lang		= "2月";
+$month_mar_lang		= "3月";
+$month_apr_lang		= "4月";
+$month_may_lang		= "5月";
+$month_jun_lang		= "6月";
+$month_jul_lang		= "7月";
+$month_aug_lang		= "8月";
+$month_sep_lang		= "9月";
+$month_oct_lang		= "10月";
+$month_nov_lang		= "11月";
+$month_dec_lang		= "12月";
+
+
+$day_sun_short_lang	= "日曜";
+$day_mon_short_lang	= "月曜";		
+$day_tue_short_lang	= "火曜";
+$day_wed_short_lang	= "水曜";
+$day_thu_short_lang	= "木曜";
+$day_fri_short_lang	= "金曜";
+$day_sat_short_lang	= "土曜";
+
+$day_sun_lang		= "日曜日";
+$day_mon_lang		= "月曜日";		
+$day_tue_lang		= "火曜日";
+$day_wed_lang		= "水曜日";
+$day_thu_lang		= "木曜日";
+$day_fri_lang		= "金曜日";
+$day_sat_lang		= "土曜日";
+
+?>
