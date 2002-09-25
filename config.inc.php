8a587b7052a1a53ecb153bc295639ed2b3e735fc
jwangen <jwangen>
2002-09-25 21:44:49 +0000

    Modified Japanese translation

--- config.inc.php
+++ config.inc.php
@@ -1,5 +1,6 @@
 <?php
 
+
 $style_sheet = "default.css";								// Themes support - "default.css", "red.css", "green.css"
 $fullpath = "webcal://path/to/your/webcal/calendars/";		// what iCal file are we using
 $calendar_path = "./calendars";								// path to directory with calendars
--- languages/japanese.inc.php
+++ languages/japanese.inc.php
@@ -1,7 +1,7 @@
 <?php
 
 // English language include
-// For version 0.4 PHP iCalendar
+// For version 0.5 PHP iCalendar
 //
 // Translation by Jared Wangen (jared@silter.org)
 //
@@ -23,58 +23,29 @@ $last_month_lang	= "先月";
 $last_week_lang		= "先週";
 $last_year_lang		= "去年";
 $subscribe_lang		= "追加";
-$download_lang		= "ダウンロド";
+$download_lang		= "ダウンロード";
 $powered_by_lang 	= "Powered by";
 
-$month_jan_lang		= "1月";
-$month_feb_lang		= "2月";
-$month_mar_lang		= "3月";
-$month_apr_lang		= "4月";
-$month_may_lang		= "5月";
-$month_jun_lang		= "6月";
-$month_jul_lang		= "7月";
-$month_aug_lang		= "8月";
-$month_sep_lang		= "9月";
-$month_oct_lang		= "10月";
-$month_nov_lang		= "11月";
-$month_dec_lang		= "12月";
-
-
-$day_sun_short_lang	= "日曜";
-$day_mon_short_lang	= "月曜";		
-$day_tue_short_lang	= "火曜";
-$day_wed_short_lang	= "水曜";
-$day_thu_short_lang	= "木曜";
-$day_fri_short_lang	= "金曜";
-$day_sat_short_lang	= "土曜";
-
-$day_sun_lang		= "日曜日";
-$day_mon_lang		= "月曜日";		
-$day_tue_lang		= "火曜日";
-$day_wed_lang		= "水曜日";
-$day_thu_lang		= "木曜日";
-$day_fri_lang		= "金曜日";
-$day_sat_lang		= "土曜日";
-
 $version_lang		= "0.5";
-$event_lang			= "Event";
-$event_start_lang	= "Start Time";
-$event_end_lang		= "End Time";
-$this_months_lang	= "This Month's Events";
-$date_lang			= "Date";
-$summary_lang		= "Summary";
+$event_lang		= "件名";
+$event_start_lang	= "開始時刻";
+$event_end_lang		= "終了時刻";
+$this_months_lang	= "今月の予定";
+$date_lang		= "月日";
+$summary_lang		= "話題";
+$all_day_lang		= "終日イベント";
 
 // Set Location for date formatting, check out: http://www.php.net/manual/en/function.setlocale.php
-setlocale (LC_TIME, 'en_EN');
+setlocale (LC_TIME, 'ja_JP.SJIS');
 
 // For time formatting, check out: http://www.php.net/manual/en/function.date.php
-$timeFormat = "g:i A";
+$timeFormat = "g:iA";
 
 // For date formatting, cehck out: http://www.php.net/manual/en/function.strftime.php
-$dateFormat_day = "%A, %B %e";
+$dateFormat_day = "%B %e %A";
 $dateFormat_week = "%B %e";
-$dateFormat_week_list = "%a, %b %e";
-$dateFormat_month = "%B %Y";
-$dateFormat_month_list = "%A, %B %e";
+$dateFormat_week_list = "%b %e %a";
+$dateFormat_month = "%Y %B";
+$dateFormat_month_list = "%B %e %A";
 
 ?>
