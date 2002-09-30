8973b748535409a51987c391c5c892c89d80269f
Chad Little <clittle@users.sourceforge.net>
2002-09-30 07:19:37 +0000

    set to off

--- config.inc.php
+++ config.inc.php
@@ -9,6 +9,6 @@ $week_start_day = "sunday";				// Day of the week your week starts on
 $use_sessions = "yes";					// For speedy performance on web servers, not good for localhost use.
 $day_start = "0700";					// Start time for day grid
 $gridLength = "15";						// grid distance in minutes for day view
-$display_custom_goto = "yes";			// in the "Jump To" box, display the custom "go to day" box. "yes" means Yes, anything else means no. "yes" must be lowercase.
+$display_custom_goto = "";				// in the "Jump To" box, display the custom "go to day" box. "yes" means Yes, anything else means no. "yes" must be lowercase.
 
 ?>
