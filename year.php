3e452f25f2592e2fdb548629f70512a1feba1e9e
Chad Little <clittle@users.sourceforge.net>
2002-10-09 01:47:01 +0000

    initial outline of year.php

---
+++ year.php
@@ -0,0 +1,113 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+        "http://www.w3.org/TR/1999/REC-html401-19991224/loose.dtd">
+<html>
+<head>
+	<meta http-equiv="content-type" content="text/html; charset=utf-8">
+	<title>Year</title>
+	<meta name="generator" content="BBEdit 6.5.3">
+</head>
+<body>
+<center>
+<table border="0" width="670" cellspacing="0" cellpadding="0">
+	<tr>
+		<td width="210">
+			<table border="1" width="210" cellspacing="0" cellpadding="0">
+				<tr>
+					<td colspan="7" align="center">January</td>
+				</tr>
+				<tr>
+					<td><img src="images/spacer.gif" width="30" height="3" alt=""></td>
+					<td><img src="images/spacer.gif" width="30" height="3" alt=""></td>
+					<td><img src="images/spacer.gif" width="30" height="3" alt=""></td>
+					<td><img src="images/spacer.gif" width="30" height="3" alt=""></td>
+					<td><img src="images/spacer.gif" width="30" height="3" alt=""></td>
+					<td><img src="images/spacer.gif" width="30" height="3" alt=""></td>
+					<td><img src="images/spacer.gif" width="30" height="3" alt=""></td>
+				</tr>
+				<tr height="30">
+					<td width="30" height="30">1</td>
+					<td width="30" height="30">2</td>
+					<td width="30" height="30">3</td>
+					<td width="30" height="30">4</td>
+					<td width="30" height="30">5</td>
+					<td width="30" height="30">6</td>
+					<td width="30" height="30">7</td>
+				</tr>
+				<tr height="30">
+					<td width="30" height="30">8</td>
+					<td width="30" height="30">9</td>
+					<td width="30" height="30">10</td>
+					<td width="30" height="30">11</td>
+					<td width="30" height="30">12</td>
+					<td width="30" height="30">13</td>
+					<td width="30" height="30">14</td>
+				</tr>
+				<tr height="30">
+					<td width="30" height="30">15</td>
+					<td width="30" height="30">16</td>
+					<td width="30" height="30">17</td>
+					<td width="30" height="30">18</td>
+					<td width="30" height="30">19</td>
+					<td width="30" height="30">20</td>
+					<td width="30" height="30">21</td>
+				</tr>
+				<tr height="30">
+					<td width="30" height="30">22</td>
+					<td width="30" height="30">23</td>
+					<td width="30" height="30">24</td>
+					<td width="30" height="30">25</td>
+					<td width="30" height="30">26</td>
+					<td width="30" height="30">27</td>
+					<td width="30" height="30">28</td>
+				</tr>
+				<tr height="30">
+					<td width="30" height="30">29</td>
+					<td width="30" height="30">30</td>
+					<td width="30" height="30">31</td>
+					<td width="30" height="30">&nbsp;</td>
+					<td width="30" height="30">&nbsp;</td>
+					<td width="30" height="30">&nbsp;</td>
+					<td width="30" height="30">&nbsp;</td>
+				</tr>
+			</table>
+		</td>
+		<td width="20"><img src="images/spacer.gif" width="20" height="1" alt=""></td>
+		<td width="210"></td>
+		<td width="20"><img src="images/spacer.gif" width="20" height="1" alt=""></td>
+		<td width="210"></td>
+	</tr>
+	<tr>
+		<td colspan="5"><img src="images/spacer.gif" width="1" height="20" alt=""></td>
+	</tr>
+	<tr>
+		<td width="210"></td>
+		<td width="20"><img src="images/spacer.gif" width="20" height="1" alt=""></td>
+		<td width="210"></td>
+		<td width="20"><img src="images/spacer.gif" width="20" height="1" alt=""></td>
+		<td width="210"></td>
+	</tr>
+	<tr>
+		<td colspan="5"><img src="images/spacer.gif" width="1" height="20" alt=""></td>
+	</tr>
+	<tr>
+		<td width="210"></td>
+		<td width="20"><img src="images/spacer.gif" width="20" height="1" alt=""></td>
+		<td width="210"></td>
+		<td width="20"><img src="images/spacer.gif" width="20" height="1" alt=""></td>
+		<td width="210"></td>
+	</tr>
+	<tr>
+		<td colspan="5"><img src="images/spacer.gif" width="1" height="20" alt=""></td>
+	</tr>
+	<tr>
+		<td width="210"></td>
+		<td width="20"><img src="images/spacer.gif" width="20" height="1" alt=""></td>
+		<td width="210"></td>
+		<td width="20"><img src="images/spacer.gif" width="20" height="1" alt=""></td>
+		<td width="210"></td>
+	</tr>
+</table>
+
+</center>
+</body>
+</html>
