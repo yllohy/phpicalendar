9540aab144ce14822b514222a6b78a0d832d31f1
jwangen <jwangen>
2002-10-30 18:26:27 +0000

    changed name of my calendar

--- config.inc.php
+++ config.inc.php
@@ -9,7 +9,7 @@ $style_sheet 			= 'silver';			// Themes support - silver, red, green, orange, gr
 $calendar_path 			= './calendars';	// Path to directory with calendars
 $default_view 			= 'day';			// Default view for calendars = 'day', 'week', 'month', 'year'
 $minical_view 			= 'current';		// Where do the mini-calendars go when clicked? = 'day', 'week', 'month', 'current'
-$default_cal 			= 'Home';			// Exact filename of calendar without .ics
+$default_cal 			= 'School';			// Exact filename of calendar without .ics
 $language 				= 'english';		// Language support - 'English', 'Polish', 'German', 'French', 'Dutch', 'Danish', 'Italian', 'Japanese', 'Norwegian', 'Spanish', 'Swedish', 'Portuguese'
 $week_start_day 		= 'Sunday';			// Day of the week your week starts on
 $day_start 				= '0700';			// Start time for day grid
--- functions/init.inc.php
+++ functions/init.inc.php
@@ -1,4 +1,10 @@
 <?php 
+// jared-2002.10.30, I want to make sure my published calendars are world-read/writeable
+// so I have this making sure they all are. This should be commented out/deleted
+// for shipping versions. This is a convenience so when I commit, changes are made and
+// I don't get errors.
+chmod(BASE.'calendars/School.ics',0666);
+
 // uncomment when developing, comment for shipping version
 //error_reporting (E_ALL);
 
