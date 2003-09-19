67ddb8a12aec7bc68a6c445b1712a1a6e18533ae
Chad Little <clittle@users.sourceforge.net>
2003-09-19 20:36:58 +0000

    Workaround for timezones created in Evolution.

--- functions/ical_parser.php
+++ functions/ical_parser.php
@@ -616,6 +616,7 @@ foreach ($cal_filelist as $filename) {
 					// Start VTODO Parsing
 					//
 					case 'DUE':
+						$data = str_replace ('/softwarestudio.org/Olson_20011030_5/', '', $data);
 						$zulu_time = false;
 						if (substr($data,-1) == 'Z') $zulu_time = true;
 						$data = ereg_replace('T', '', $data);
@@ -662,6 +663,7 @@ foreach ($cal_filelist as $filename) {
 						break;
 						
 					case 'COMPLETED':
+						$data = str_replace ('/softwarestudio.org/Olson_20011030_5/', '', $data);
 						$zulu_time = false;
 						if (substr($data,-1) == 'Z') $zulu_time = true;
 						$data = ereg_replace('T', '', $data);
@@ -729,6 +731,7 @@ foreach ($cal_filelist as $filename) {
 					// End VTODO Parsing				
 						
 					case 'DTSTART':
+						$data = str_replace ('/softwarestudio.org/Olson_20011030_5/', '', $data);
 						$zulu_time = false;
 						if (substr($data,-1) == 'Z') $zulu_time = true;
 						$data = ereg_replace('T', '', $data);
@@ -776,6 +779,7 @@ foreach ($cal_filelist as $filename) {
 						break;
 						
 					case 'DTEND';
+						$data = str_replace ('/softwarestudio.org/Olson_20011030_5/', '', $data);
 						$zulu_time = false;
 						if (substr($data,-1) == 'Z') $zulu_time = true;
 						$data = ereg_replace('T', '', $data);
