51ddfbbb8e5c9286440a4dccd75bb637349a8f43
Chad Little <clittle@users.sourceforge.net>
2005-10-02 22:34:11 +0000

    Finnish

--- languages/finnish.inc.php
+++ languages/finnish.inc.php
@@ -2,128 +2,141 @@
 
 // Finnish language include
 // For version 1.0 PHP iCalendar
-//
 // Translation by Kari Somby (kari.somby@pp.inet.fi)
+//
+// Revice for version 2.0.1 PHP iCalendar
+// Text and script update by Matti Monnonen (matti@monnonen.com)
 //
 // Submit new translations to chad@chadsdomain.com
 
-$day_lang		= 'P&auml;iv&auml;';
-$week_lang		= 'Viikko';
-$month_lang		= 'Kuukausi';
-$year_lang		= 'Vuosi';
-$calendar_lang		= 'Kalenteri';
-$next_day_lang		= 'Seuraava P&auml;iv&auml;';
-$next_month_lang	= 'Seuraava Kuukausi';
-$next_week_lang		= 'Seuraava Viikko';
-$next_year_lang		= 'Seuraava Vuosi';
-$last_day_lang		= 'Edellinen P&auml;iv&auml;';
-$last_month_lang	= 'Edellinen Kuukausi';
-$last_week_lang		= 'Edellinen Viikko';
-$last_year_lang		= 'Edellinen Vuosi';
-$subscribe_lang		= 'Tilaa';
-$download_lang		= 'Lataa tiedosto';
-$powered_by_lang 	= 'Powered by';
-$event_lang		= 'Tapahtuma';
-$event_start_lang	= 'Aloitusaika';
-$event_end_lang		= 'Lopetusaika';
-$this_months_lang	= 'T&auml;m&auml;n Kuukauden tapahtumat';
-$date_lang		= 'P&auml;iv&auml;';
-$summary_lang		= 'Yhteenveto';
-$all_day_lang		= 'Koko p&auml;iv&auml;n tapahtuma';
-$notes_lang		= 'Muistiinpanot';
-$this_years_lang	= 'T&auml;m&auml;n Vuoden tapahtumat';
-$today_lang		= 'Today';
-$this_week_lang		= 'T&auml;m&auml; Viikko';
-$this_month_lang	= 'T&auml;m&auml; Kuukausi';
-$jump_lang		= 'Siirry';
-$tomorrows_lang		= 'Huomisen Tapahtumat';
-$goday_lang		= 'Mene T&auml;h&auml;n P&auml;iv&auml;&auml;n';
-$goweek_lang		= 'Mene T&auml;h&auml;n Viikkoon';
-$gomonth_lang		= 'Mene T&auml;h&auml;n Kuukauteen';
-$goyear_lang		= 'Mene T&auml;h&auml;n Vuoteen';
-$search_lang		= 'Etsi'; // the verb
-$results_lang		= 'Etsinn&auml;n Tulokset';
-$query_lang		= 'Kysely'; // will be followed by the search query
-$no_results_lang	= 'Tapahtumia ei l&ouml;ytynyt';
-$goprint_lang		= 'Tulostettava versio';
-$time_lang		= 'Aika';
-$summary_lang		= 'Yhteenveto';
-$description_lang	= 'Selitys';
-$this_site_is_lang	= 'T&auml;m&auml; sivusto on';
-$no_events_day_lang	= 'Ei tapahtumia t&auml;n&auml;&auml;n.';
-$no_events_week_lang	= 'Ei tapahtumia t&auml;ll&auml; viikolla.';
-$no_events_month_lang	= 'Ei tapahtumia t&auml;ss&auml; kuussa.';
-$rss_day_date		= 'g:i A';  // Lists just the time
-$rss_week_date		= '%b %e';  // Lists just the day
-$rss_month_date		= '%b %e';  // Lists just the day
-$rss_language		= 'fi-fi';
-$search_took_lang	= 'Etsimiseen meni %s sekuntia';
-$recurring_event_lang	= 'Toistuva tapahtuma';
-$exception_lang		= 'Poikkeus';
-$no_query_lang		= 'Kysely&auml; ei annettu';
-$preferences_lang	= 'Ominaisuudet';
-$printer_lang		= 'Tulostin';
-$select_lang_lang	= 'Valitse oletuskieli:';
-$select_cal_lang	= 'Valitse oletuskalenteri:';
-$select_view_lang	= 'Valitse oletusn&auml;kym&auml;:';
-$select_time_lang	= 'Valitse oletusaloitusaika:';
-$select_day_lang	= 'Valitse oletusaloitusp&auml;iv&auml; viikolle:';
-$select_style_lang	= 'Valitse oletustyyli:';
-$set_prefs_lang		= 'Aseta ominaisuudet';
-$completed_date_lang	= 'Valmistuu';
-$completed_lang		= 'Valmis';
-$created_lang		= 'Luotu:';
-$due_lang		= 'Er&auml;&auml;ntyy:';
-$priority_lang		= 'T&auml;rkeys:';
-$priority_high_lang	= 'Korkea';
-$priority_low_lang	= 'Matala';
-$priority_medium_lang	= 'Keski';
-$priority_none_lang	= 'Ei mit&auml;&auml;n';
-$status_lang		= 'Tila';
-$todo_lang		= 'Teht&auml;v&auml;t:';
-$unfinished_lang	= 'P&auml;&auml;ttym&auml;tt&ouml;m&auml;t';
-$prefs_set_lang 	= 'Asetukset p&auml;ivitetty';
-$prefs_unset_lang 	= 'Asetuksia ei p&auml;ivitetty. Muutokset p&auml;ivitet&auml;&auml;n seuraavan sivun ladattua';
-$unset_prefs_lang 	= 'Asetukset peruttu';
-$organizer_lang		= 'Organisoija';
-$attendee_lang		= 'Osanottajat';
-$location_lang		= 'Paikka';
-$admin_header_lang	= 'PHP iCalendar Hallinta';
-$username_lang		= 'K&aumlytt&aumlj&aumltunnus';
-$password_lang		= 'Salasana';
-$login_lang		= 'Kirjaudu sis&auml&aumln';
-$invalid_login_lang	= 'V&auml&aumlr&auml k&aumlytt&aumlj&aumltunnus tai salasana.';
-$addupdate_cal_lang	= 'Lis&auml&auml tai p&aumlivit&auml kalenteri';
-$addupdate_desc_lang	= 'Lis&auml&auml kalenteri lataamalla uusi tiedosto. P&aumlivit&auml kalenteri samannimisell&auml tiedostolla';
-$delete_cal_lang	= 'Tuhoa kalenteri';
-$logout_lang		= 'Kirjaudu ulos';
-$cal_file_lang		= 'Kalenteritiedosto';
-$php_error_lang		= 'PHP virhe';
-$upload_error_gen_lang	= 'Tiedoston latauksessa oli ongelma.';
-$upload_error_lang[0]	= 'Tiedoston latauksessa oli ongelma.';
-$upload_error_lang[1]	= 'Ladattavan tiedoston koko on liian suuri.';
-$upload_error_lang[2]	= 'Ladattavan tiedoston koko on liian suuri';
-$upload_error_lang[3]	= 'Tiedoston lataus onnistui vain osittain.';
-$upload_error_lang[4]	= 'Valitse ladattava tiedosto.';
-$upload_error_type_lang = 'Vain .ics tiedostoja voidaan ladata.';
-$copy_error_lang	= 'Tiedoston kopiointi ep&aumlonnistui.';
-$delete_error_lang	= 'Tiedoston tuhoaminen ep&aumlonnistui.';
-$delete_success_lang	= 'Tuhoaminen onnistui.';
-$action_success_lang	= 'Toiminta onnistui.';
-$submit_lang		= 'L&aumlhet&auml';
-$delete_lang		= 'Tuhoa';
+$lang['l_day']			= 'P&auml;iv&auml;';
+$lang['l_week']			= 'Viikko';
+$lang['l_month']		= 'Kuukausi';
+$lang['l_year']			= 'Vuosi';
+$lang['l_calendar']		= 'Kalenteri';
+$lang['l_next_day']		= 'Seuraava P&auml;iv&auml;';
+$lang['l_next_month']		= 'Seuraava Kuukausi';
+$lang['l_next_week']		= 'Seuraava Viikko';
+$lang['l_next_year']		= 'Seuraava Vuosi';
+$lang['l_last_day']		= 'Edellinen P&auml;iv&auml;';
+$lang['l_last_month']		= 'Edellinen Kuukausi';
+$lang['l_last_week']		= 'Edellinen Viikko';
+$lang['l_last_year']		= 'Edellinen Vuosi';
+$lang['l_subscribe']		= 'Kopioi kalenteri';
+$lang['l_download']		= 'Lataa tiedosto';
+$lang['l_powered_by'] 		= 'Powered by';
+$lang['l_event']		= 'Tapahtuma';
+$lang['l_event_start']		= 'Aloitusaika';
+$lang['l_event_end']		= 'Lopetusaika';
+$lang['l_this_months']		= 'T&auml;m&auml;n Kuukauden tapahtumat';
+$lang['l_date']			= 'P&auml;iv&auml;';
+$lang['l_summary']		= 'Yhteenveto';
+$lang['l_all_day']		= 'Koko p&auml;iv&auml;n tapahtuma';
+$lang['l_notes']		= 'Muistiinpanot';
+$lang['l_this_years']		= 'T&auml;m&auml;n Vuoden tapahtumat';
+$lang['l_today']		= 'T&auml;n&auml;&auml;n';
+$lang['l_this_week']		= 'T&auml;m&auml; Viikko';
+$lang['l_this_month']		= 'T&auml;m&auml; Kuukausi';
+$lang['l_jump']			= 'Siirry';
+$lang['l_tomorrows']		= 'Huomisen Tapahtumat';
+$lang['l_goday']		= 'Mene T&auml;h&auml;n P&auml;iv&auml;&auml;n';
+$lang['l_goweek']		= 'Mene T&auml;h&auml;n Viikkoon';
+$lang['l_gomonth']		= 'Mene T&auml;h&auml;n Kuukauteen';
+$lang['l_goyear']		= 'Mene T&auml;h&auml;n Vuoteen';
+$lang['l_search']		= 'Etsi'; // the verb
+$lang['l_results']		= 'Etsinn&auml;n Tulokset';
+$lang['l_query']		= 'Kysely'; // will be followed by the search query
+$lang['l_no_results']		= 'Tapahtumia ei l&ouml;ytynyt';
+$lang['l_goprint']		= 'Tulostettava versio';
+$lang['l_time']			= 'Aika';
+$lang['l_summary']		= 'Yhteenveto';
+$lang['l_description']		= 'Selitys';
+$lang['l_this_site_is']		= 'T&auml;m&auml; sivusto on';
+$lang['l_no_events_day']	= 'Ei tapahtumia t&auml;n&auml;&auml;n.';
+$lang['l_no_events_week']	= 'Ei tapahtumia t&auml;ll&auml; viikolla.';
+$lang['l_no_events_month']	= 'Ei tapahtumia t&auml;ss&auml; kuussa.';
+$lang['l_rss_day']		= 'g:i A';  // Lists just the time
+$lang['l_rss_week_date']	= '%b %e';  // Lists just the day
+$lang['l_rss_month_date']	= '%b %e';  // Lists just the day
+$lang['l_rss_language']		= 'fi-fi';
+$lang['l_search_took']		= 'Etsimiseen meni %s sekuntia';
+$lang['l_recurring_event']	= 'Toistuva tapahtuma';
+$lang['l_exception']		= 'Poikkeus';
+$lang['l_no_query']		= 'Kysely&auml; ei annettu';
+$lang['l_preferences']		= 'Ominaisuudet';
+$lang['l_printer']		= 'Tulostin';
+$lang['l_select_lang']		= 'Valitse oletuskieli:';
+$lang['l_select_cal']		= 'Valitse oletuskalenteri:';
+$lang['l_select_view']		= 'Valitse oletusn&auml;kym&auml;:';
+$lang['l_select_time']		= 'Valitse oletusaloitusaika:';
+$lang['l_select_day']		= 'Valitse oletusaloitusp&auml;iv&auml; viikolle:';
+$lang['l_select_style']		= 'Valitse oletustyyli:';
+$lang['l_t_prefs']		= 'Aseta ominaisuudet';
+$lang['l_completed_date']	= 'Valmistuu';
+$lang['l_completed']		= 'Valmis';
+$lang['l_created']		= 'Luotu:';
+$lang['l_due']			= 'Er&auml;&auml;ntyy:';
+$lang['l_priority']		= 'T&auml;rkeys:';
+$lang['l_priority_high']	= 'Korkea';
+$lang['l_priority_low']		= 'Matala';
+$lang['l_priority_medium']	= 'Keski';
+$lang['l_priority_none']	= 'Ei mit&auml;&auml;n';
+$lang['l_status']		= 'Tila';
+$lang['l_todo']			= 'Teht&auml;v&auml;t:';
+$lang['l_unfinished']		= 'P&auml;&auml;ttym&auml;tt&ouml;m&auml;t';
+$lang['l_unfinished']		= 'P&auml;&auml;ttym&auml;tt&ouml;m&auml;t';
+$lang['l_prefs_set'] 		= 'Asetukset p&auml;ivitetty';
+$lang['l_prefs_unset'] 		= 'Asetuksia ei p&auml;ivitetty. Muutokset p&auml;ivitet&auml;&auml;n seuraavan sivun ladattua';
+$lang['l_unset_prefs'] 		= 'Asetukset peruttu';
+$lang['l_organizer']		= 'Organisoija';
+$lang['l_attendee']		= 'Osanottajat';
+$lang['l_location']		= 'Paikka';
+$lang['l_admin_header']		= 'PHP iCalendar Hallinta';
+$lang['l_username']		= 'K&auml;ytt&auml;j&auml;tunnus';
+$lang['l_password']		= 'Salasana';
+$lang['l_login']		= 'Kirjaudu sis&auml;&auml;n';
+$lang['l_invalid_login']	= 'V&auml;&auml;r&auml; k&auml;ytt&auml;j&auml;tunnus tai salasana.';
+$lang['l_addupdate_cal']	= 'Lis&auml&auml; tai p&auml;ivit&auml; kalenteri';
+$lang['l_addupdate_desc']	= 'Lis&auml&auml; kalenteri lataamalla uusi tiedosto. P&aumlivit&auml kalenteri samannimisell&auml tiedostolla';
+$lang['l_delete_cal']		= 'Tuhoa kalenteri';
+$lang['l_logout']		= 'Kirjaudu ulos';
+$lang['l_cal_file']		= 'Kalenteritiedosto';
+$lang['l_php_error']		= 'PHP virhe';
+$lang['l_upload_error_gen']	= 'Tiedoston latauksessa oli ongelma.';
+$upload_error_lang[0]		= 'Tiedoston latauksessa oli ongelma.';
+$upload_error_lang[1]		= 'Ladattavan tiedoston koko on liian suuri.';
+$upload_error_lang[2]		= 'Ladattavan tiedoston koko on liian suuri';
+$upload_error_lang[3]		= 'Tiedoston lataus onnistui vain osittain.';
+$upload_error_lang[4]		= 'Valitse ladattava tiedosto.';
+$lang['l_upload_error_type'] 	= 'Voit ladata vain .ics tiedostoja.';
+$lang['l_copy_error']		= 'Tiedoston kopiointi ep&auml;onnistui.';
+$lang['l_delete_error']		= 'Tiedoston tuhoaminen ep&auml;onnistui.';
+$lang['l_delete_success']	= 'Tuhoaminen onnistui.';
+$lang['l_action_success']	= 'Toiminta onnistui.';
+$lang['l_submit']		= 'L&auml;het&auml;';
+$lang['l_delete']		= 'Tuhoa';
 
 // ----- New for 0.9.5
-$all_cal_comb_lang	= 'Kaikki kalenterit yhdistetty';
+$all_cal_comb_lang              = 'Kaikki yhdistettyn&auml;';
+
+// ---- New for 2.0.1
+$lang['l_legend']		= 'Otsikko';
+$lang['l_admin_subhead']	= 'Kalenterin hallinta sivu';
+$lang['l_prefs_subhead']	= 'Keksin asetukset vierailijoille';
+$lang['l_rss_info']		= 'RSS Tiedot';
+$lang['l_rss_subhead']		= 'Perus RSS tiedot jokaiselle kalenterille';
+$lang['l_rss_notenabled']	= 'RSS ei ole k&auml;yt&ouml;ss&auml; t&auml;ll&auml; sivulla';
+
+
 
 // - navigation
-$back_lang 		= 'Takaisin';
-$next_lang 		= 'Seuraava';
-$prev_lang 		= 'Edellinen';
-$day_view_lang 		= 'P&auml;iv&auml;n&auml;kym&auml;';
-$week_view_lang 	= 'Viikkon&auml;kym&auml;';
-$month_view_lang 	= 'Kuukausin&auml;kym&auml;';
-$year_view_lang 	= 'Vuosin&auml;kym&auml;';
+$lang['l_back'] 		= 'Takaisin';
+$lang['l_next'] 		= 'Seuraava';
+$lang['l_prev'] 		= 'Edellinen';
+$lang['l_day_view'] 		= 'P&auml;iv&auml;n&auml;kym&auml;';
+$lang['l_week_view'] 		= 'Viikkon&auml;kym&auml;';
+$lang['l_month_view'] 		= 'Kuukausin&auml;kym&auml;';
+$lang['l_year_view'] 		= 'Vuosin&auml;kym&auml;';
 
 // ---------------------------------
 
@@ -131,23 +144,22 @@ $year_view_lang 	= 'Vuosin&auml;kym&auml;';
 
 
 // $format_recur, items enclosed in % will be substituted with variables
-$format_recur_lang['delimiter']	= ', ';								// ie, 'one, two, three'
+$format_recur_lang['delimiter']	= ', ';						// ie, 'one, two, three'
 
 $format_recur_lang['yearly']		= array('vuosi','vuotta');		// for these, put singular
-$format_recur_lang['monthly']		= array('kuukausi','kuukautta');		// and plural forms
+$format_recur_lang['monthly']		= array('kuukausi','kuukautta');	// and plural forms
 $format_recur_lang['weekly']		= array('viikko','viikkoa');		// these will be %freq%
-$format_recur_lang['daily']			= array('p&auml;iv&auml;','p&auml;iv&auml;&auml;');			// in the replacement below
+$format_recur_lang['daily']		= array('p&auml;iv&auml;','p&auml;iv&auml;&auml;');			// in the replacement below
 $format_recur_lang['hourly']		= array('tunti','tuntia');
 $format_recur_lang['minutely']		= array('minuutti','minuuttia');
 $format_recur_lang['secondly']		= array('sekuntti','sekunttia');
 
 $format_recur_lang['start']		= 'Joka %int% %freq% %for%';	// ie, 'Every 1 day until January 4' or 'Every 1 day for a count of 5'
-$format_recur_lang['until']		= '%date% asti';				// ie, 'until January 4'
+$format_recur_lang['until']		= '%date% asti';		// ie, 'until January 4'
 $format_recur_lang['count']		= '%int% kertaa';		// ie, 'for 5 times'
 
 $format_recur_lang['bymonth']		= 'Kuukaudet: %list%';			// ie, 'In months: January, February, March'
-$format_recur_lang['bymonthday']	= 'P&auml;iv&auml;t: %list%';			// ie, 'On dates: 1, 2, 3, 4'
-$format_recur_lang['byday']			= 'Viikonp&auml;iv&auml;t: %list%';			// ie, 'On days: Mon, Tues, Wed, Thurs'
+$format_recur_lang['byday']		= 'Viikonp&auml;iv&auml;t: %list%';	// ie, 'On days: Mon, Tues, Wed, Thurs'
 
 $daysofweek_lang			= array ('Sunnuntai','Maanantai','Tiistai','Keskiviikko','Torstai','Perjantai','Lauantai');
 $daysofweekshort_lang			= array ('Su','Ma','Ti','Ke','To','Pe','La');
@@ -185,13 +197,13 @@ Notes about dateFormat_*
 */
 
 // Error messages - %s will be replaced with a variable
-$error_title_lang 		= 'Virhe!';
-$error_window_lang 		= 'Tapahtui virhe!';
-$error_calendar_lang 		= 'Kalenteria "%s" k&auml;siteltiin virheen sattuessa.';
-$error_path_lang 		= 'Polkua: "%s" ei voi avata';
-$error_back_lang 		= 'Ole hyv&auml; ja k&auml;yt&auml; "Takaisin" nappia palataksesi takaisin.';
-$error_remotecal_lang 		= 'T&auml;m&auml; palvelin sallii vain hyv&auml;ksyttyjen et&auml;kalentereiden k&auml;yt&ouml;n.';
-$error_restrictedcal_lang 	= 'Yritit k&auml;ytt&auml;&auml; kalenteria, jonka k&auml;ytt&ouml; on rajoitettu.';
-$error_invalidcal_lang 		= 'Ei kelvollinen kalenterin nimi. Ole hyv&auml; ja yrit&auml; toista kalenteria.';
+$lang['l_error_title'] 			= 'Virhe!';
+$lang['l_error_window'] 		= 'Tapahtui virhe!';
+$lang['l_error_calendar'] 		= 'Kalenteria "%s" k&auml;siteltiin virheen sattuessa.';
+$lang['l_error_path'] 			= 'Polkua: "%s" ei voi avata';
+$lang['l_error_back'] 			= 'Ole hyv&auml; ja k&auml;yt&auml; "Takaisin" nappia palataksesi takaisin.';
+$lang['l_error_remotecal'] 		= 'T&auml;m&auml; palvelin sallii vain hyv&auml;ksyttyjen et&auml;kalentereiden k&auml;yt&ouml;n.';
+$lang['l_error_restrictedcal'] 		= 'Yritit k&auml;ytt&auml;&auml; kalenteria, jonka k&auml;ytt&ouml; on rajoitettu.';
+$lang['l_error_invalidcal'] 		= 'Ei kelvollinen kalenterin nimi. Ole hyv&auml; ja yrit&auml; toista kalenteria.';
 
 ?>
