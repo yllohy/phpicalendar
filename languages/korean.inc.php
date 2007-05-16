b2f06e274cf29e486440224612d5b4a23e95b225
Jim Hu <jimhu@users.sourceforge.net>
2007-05-16 23:35:25 +0000

    new language files

--- languages/korean.inc.php
+++ languages/korean.inc.php
@@ -1,129 +1,139 @@
 ﻿<?php
 
-// Korean language include
-// For version 1.0 PHP iCalendar
+// English language include
+// For version 2.0 PHP iCalendar
 //
-// Translation by TaeYoung Oh (spier@hybird.net)
+// Translation by Sang-un Chang (gomibak@gmail.com)
 //
-// Submit new translations to chad@chadsdomain.com
+// Submit new translations to gomibak@gmail.com
 
-$day_lang			= '일일';
-$week_lang			= '주간';
-$month_lang			= '월간';
-$year_lang			= '연간';
-$calendar_lang		= '달력';
-$next_day_lang		= '다음 날';
-$next_month_lang	= '다음 달';
-$next_week_lang		= '다음 주';
-$next_year_lang		= '다음 해';
-$last_day_lang		= '전날';
-$last_month_lang	= '전 달';
-$last_week_lang		= '전 주';
-$last_year_lang		= '전 년';
-$subscribe_lang		= '가입';
-$download_lang		= '다운로드';
-$powered_by_lang 	= 'Powered by';
-$event_lang			= '일정';
-$event_start_lang	= '시작시간';
-$event_end_lang		= '종료시간';
-$this_months_lang	= '이달의 일정';
-$date_lang			= '날짜';
-$summary_lang		= '요약';
-$all_day_lang		= '하루종일';
-$notes_lang			= '메모';
-$this_years_lang	= '올해의 일정';
-$today_lang			= '오늘';
-$this_week_lang		= '금주';
-$this_month_lang	= '이번달';
-$jump_lang			= '이동';
-$tomorrows_lang		= '내일의 일정';
-$goday_lang			= '일일 보기';
-$goweek_lang		= '주간 보기';
-$gomonth_lang		= '월간 보기';
-$goyear_lang		= '연간 보기';
-$search_lang		= '검색'; // the verb
-$results_lang		= '검색결과';
-$query_lang			= '질의어'; // will be followed by the search query
-$no_results_lang	= '검색 결과가 없습니다.';
-$goprint_lang		= '인쇄용 보기';
-$time_lang			= '시간';
-$summary_lang		= '요약';
-$description_lang	= '설명';
-$this_site_is_lang		= 'This site is';
-$no_events_day_lang		= '오늘의 일정이 없습니다.';
-$no_events_week_lang	= '이번주에는 일정이 없습니다.';
-$no_events_month_lang	= '이달에는 일정이 없습니다.';
-$rss_day_date			= 'g:i A';  // Lists just the time
-$rss_week_date			= '%b %e일';  // Lists just the day
-$rss_month_date			= '%b %e일';  // Lists just the day
-$rss_language			= 'ko-kr';
-$search_took_lang		= '검색소요시간 %s 초';
-$recurring_event_lang	= '반복 일정';
-$exception_lang			= '예외';
-$no_query_lang			= '질의어가 없습니다';
-$preferences_lang		= '환경설정';
-$printer_lang			= '프린터';
-$select_lang_lang		= '기본 언어를 선택하세요:';
-$select_cal_lang		= '기본 달력을 선택하세요:';
-$select_view_lang		= '기본 보기를 선택하세요:';
-$select_time_lang		= '기본 시작 시간을 선택하세요:';
-$select_day_lang		= '한주의 시작요일을 선택하세요:';
-$select_style_lang		= '기본 스타일을 선택하세요:';
-$set_prefs_lang			= '환경설정 저장';
-$completed_date_lang	= '완료일';
-$completed_lang			= '완료됨';
-$created_lang			= '생성됨:';
-$due_lang				= '마감일:';
-$priority_lang			= '우선순위:';
-$priority_high_lang		= '높음';
-$priority_low_lang		= '낮음';
-$priority_medium_lang	= '중간';
-$priority_none_lang		= '없음';
-$status_lang			= '상태';
-$todo_lang				= '해야할 일';
-$unfinished_lang		= '마무리 안됨';
-$prefs_set_lang 		= '환경설정이 저장되었습니다.';
-$prefs_unset_lang 		= '환경설정 초기화되었습니다. 변경사항은 다음 페이지가 로딩될 때 적용됩니다.';
-$unset_prefs_lang 		= '환경설정 초기화:';
-$organizer_lang			= '주선자';
-$attendee_lang			= '참석자';
-$location_lang			= '위치';
-$admin_header_lang		= 'PHP iCalendar 관리';
-$username_lang			= '사용자 이름';
-$password_lang			= '비밀번호';
-$login_lang				= '로그인';
-$invalid_login_lang		= '잘못된 사용자 이름이나 비밀번호.';
-$addupdate_cal_lang		= '달력 추가 또는 갱신';
-$addupdate_desc_lang	= '새 파일을 업로드하여 달력을 추가하세요. 같은 이름의 파일을 업로드하여 달력을 갱신할 수 있습니다.';
-$delete_cal_lang		= '달력 삭제';
-$logout_lang			= '로그아웃';
-$cal_file_lang			= '달력 파일';
-$php_error_lang			= 'PHP 오류';
-$upload_error_gen_lang	= '업로드에 문제가 있었습니다.';
-$upload_error_lang[0]	= '업로드에 문제가 있었습니다.';
-$upload_error_lang[1]	= '업로드하려는 파일이 너무 큽니다.';
-$upload_error_lang[2]	= '업로드하려는 파일이 너무 큽니다.';
-$upload_error_lang[3]	= '업로드하려는 파일의 일부만 업로드되었습니다.';
-$upload_error_lang[4]	= '업로드할 파일을 선택해야합니다.';
-$upload_error_type_lang = '.ics 파일만 업로드할 수 있습니다.';
-$copy_error_lang		= '파일 복사에 실패했습니다';
-$delete_error_lang		= '파일 삭제에 실패했습니다';
-$delete_success_lang	= '(이)가 성공적으로 삭제되었습니다.';
-$action_success_lang	= '성공했습니다.';
-$submit_lang			= '제출';
-$delete_lang			= '삭제';
+$lang['l_day'] 				= '일별';
+$lang['l_week'] 			= '주별';
+$lang['l_month'] 			= '월별';
+$lang['l_year'] 			= '년간';
+$lang['l_calendar'] 		= '달력';
+$lang['l_next_day'] 		= '다음 날';
+$lang['l_next_month'] 		= '다음 달';
+$lang['l_next_week'] 		= '다음 주';
+$lang['l_next_year'] 		= '다음 해';
+$lang['l_last_day'] 		= '전 날';
+$lang['l_last_month'] 		= '지난 달';
+$lang['l_last_week'] 		= '지난 주';
+$lang['l_last_year'] 		= '작년';
+$lang['l_subscribe'] 		= '추가';
+$lang['l_download'] 		= '내려받기';
+$lang['l_powered_by'] 		= 'Powered by';
+$lang['l_event'] 			= '제목';
+$lang['l_event_start'] 		= '시작';
+$lang['l_event_end'] 		= '마침';
+$lang['l_this_months'] 		= '이번 달 예정';
+$lang['l_date'] 			= '날짜';
+$lang['l_summary'] 			= '요약';
+$lang['l_all_day'] 			= '종일 계획';
+$lang['l_notes'] 			= '참고';
+$lang['l_this_years'] 		= '올해 예정';
+$lang['l_today'] 			= '오늘';
+$lang['l_this_week'] 		= '이번 주';
+$lang['l_this_month'] 		= '이번 달';
+$lang['l_jump'] 			= '바로 가기';
+$lang['l_tomorrows'] 		= '내일 일정';
+$lang['l_goday'] 			= '오늘로 가기';
+$lang['l_goweek'] 			= '이번 주로 가기';
+$lang['l_gomonth'] 			= '이번 달로 가기';
+$lang['l_goyear'] 			= '올해로 가기';
+$lang['l_search'] 			= '검색'; // the verb
+$lang['l_results'] 			= '검색결과';
+$lang['l_pick_multiple'] 	= '복수 선택';
+$lang['l_query'] 			= '검색 키워드'; // will be followed by the search query
+$lang['l_no_results'] 		= '일치하는 일정이 없습니다.';
+$lang['l_goprint'] 			= '인쇄용 표시';
+$lang['l_time'] 			= '시간';
+$lang['l_summary'] 			= '요약';
+$lang['l_description'] 		= '메모';
+$lang['l_this_site_is']		= 'This site is';
+$lang['l_no_events_day']	= '오늘은 일정이 없습니다.';
+$lang['l_no_events_week']	= '이번 주는 예정이 없습니다.';
+$lang['l_no_events_month']	= '이번 달은 예정이 없습니다.';
+$lang['l_rss_day_date']		= 'g:i A';  // Lists just the time
+$lang['l_rss_week_date	']	= '%b %e일';  // Lists just the day
+$lang['l_rss_month_date	']	= '%b %e일';  // Lists just the day
+$lang['l_rss_language']		= 'ko';
+$lang['l_search_took']		= '검색에 %s초 걸렸습니다.';
+$lang['l_recurring_event']	= '반복 일정';
+$lang['l_exception']		= '예외';
+$lang['l_no_query']			= '검색 키워드가 없었습니다.';
+$lang['l_preferences']		= '상세설정';
+$lang['l_printer']			= '프린터';
+$lang['l_select_lang']		= '언어를 선택하세요.';
+$lang['l_select_cal']		= '달력을 선택하세요.';
+$lang['l_select_view']		= '기본 보기를 선택하세요.';
+$lang['l_select_time']		= '시작 시간을 선택하세요.';
+$lang['l_select_day']		= '시작 요일을 선택하세요.';
+$lang['l_select_style']		= '배경을 선택하세요.';
+$lang['l_set_prefs']		= '상세설정을 저장';
+$lang['l_completed_date']	= '%s에 끝났습니다.';
+$lang['l_completed']		= '마침';
+$lang['l_created']			= '작성일:';
+$lang['l_due']				= '기간:';
+$lang['l_priority']			= '우선순위:';
+$lang['l_priority_high']	= '높음';
+$lang['l_priority_low']		= '낮음';
+$lang['l_priority_medium']	= '보통';
+$lang['l_priority_none']	= '없음';
+$lang['l_status']			= '상태';
+$lang['l_status_confirmed'] = '확인됨' ; 
+$lang['l_status_cancelled'] = '취소됨'; 
+$lang['l_status_tentative'] = '임시'; 
+$lang['l_todo']				= '할 일';
+$lang['l_unfinished']		= '끝나지 않음';
+$lang['l_prefs_set'] 		= '설정이 저장되었습니다.';
+$lang['l_prefs_unset'] 		= '설정이 저장되지 않았습니다. 변경은 다음 번부터 적용됩니다.';
+$lang['l_unset_prefs'] 		= '저장한 설정을 지움';
+$lang['l_organizer']		= 'Organizer';
+$lang['l_attendee']			= 'Attendee';
+$lang['l_location']			= 'Location';
+$lang['l_admin_header']		= 'PHP iCalendar Administration';
+$lang['l_username']			= '이름';
+$lang['l_password']			= '비밀번호';
+$lang['l_login']			= '로그인';
+$lang['l_invalid_login']	= '이름 또는 비밀번호가 틀립니다.';
+$lang['l_addupdate_cal']	= 'Add or Update a Calendar';
+$lang['l_addupdate_desc']	= 'Add a calendar by uploading a new file. Update a calendar by uploading a file of the same name.';
+$lang['l_delete_cal']		= 'Delete a Calendar';
+$lang['l_logout']			= '로그아웃';
+$lang['l_cal_file']			= 'Calendar File';
+$lang['l_php_error']		= 'PHP Error';
+$upload_error_gen_lang		= '업로드에 문제가 생겼습니다.';
+$upload_error_lang[0]		= '업로드에 문제가 생겼습니다.';
+$upload_error_lang[1]		= '업로드 파일이 너무 큽니다.';
+$upload_error_lang[2]		= '업로드 파일이 너무 큽니다.';
+$upload_error_lang[3]		= '일부만이 업로드되었습니다.';
+$upload_error_lang[4]		= '업로드할 파일을 선택하세요.';
+$lang['l_upload_error_type'] = '.ics 파일만 업로드할 수 있습니다.';
+$lang['l_copy_error']			= '복사 실패';
+$lang['l_delete_error']			= '삭제 실패';
+$lang['l_delete_success']		= 'was deleted successfully.';
+$lang['l_action_success']		= 'Your action was successful.';
+$lang['l_submit']				= 'Submit';
+$lang['l_delete']				= 'Delete';
+$all_cal_comb_lang			= '모든 달력 보기';
 
-// ----- New for 0.9.4
-$all_cal_comb_lang		= '모든 달력 합쳐보기';
+// New for 2.0
+$lang['l_legend']			= '보기';
+$lang['l_admin_subhead']	= '이 페이지에서 달력을 관리합니다.';
+$lang['l_prefs_subhead']	= '이 페이지를 보기위한 쿠키를 설정합니다.';
+$lang['l_rss_info']			= 'RSS Information';
+$lang['l_rss_subhead']		= 'Basic RSS feeds available for each calendar';
+$lang['l_rss_notenabled']	= 'RSS is not enabled on this site';
 
 // - navigation
-$back_lang = '뒤로';
-$next_lang = '다음';
-$prev_lang = '이전';
-$day_view_lang = '일일 보기';
-$week_view_lang = '주간 보기';
-$month_view_lang = '월간 보기';
-$year_view_lang = '연간 보기';
+$lang['l_back'] 		= '돌아가기';
+$lang['l_next'] 		= '다음';
+$lang['l_prev'] 		= '앞';
+$lang['l_day_view'] 	= '일별';
+$lang['l_week_view'] 	= '주별';
+$lang['l_month_view'] 	= '월별';
+$lang['l_year_view'] 	= '년간';
 
 // ---------------------------------
 
@@ -131,38 +141,41 @@ $year_view_lang = '연간 보기';
 // $format_recur, items enclosed in % will be substituted with variables
 $format_recur_lang['delimiter']	= ', ';								// ie, 'one, two, three'
 
-$format_recur_lang['yearly']		= array('year','years');		// for these, put singular
-$format_recur_lang['monthly']		= array('month','months');		// and plural forms
-$format_recur_lang['weekly']		= array('week','weeks');		// these will be %freq%
-$format_recur_lang['daily']			= array('day','days');			// in the replacement below
-$format_recur_lang['hourly']		= array('hour','hours');
-$format_recur_lang['minutely']		= array('minute','minutes');
-$format_recur_lang['secondly']		= array('second','seconds');
+$format_recur_lang['yearly']		= array('년','년');		// for these, put singular
+$format_recur_lang['monthly']		= array('개월','개월');		// and plural forms
+$format_recur_lang['weekly']		= array('주','주');		// these will be %freq%
+$format_recur_lang['daily']			= array('일','일');			// in the replacement below
+$format_recur_lang['hourly']		= array('시간','시간');
+$format_recur_lang['minutely']		= array('분','분');
+$format_recur_lang['secondly']		= array('초','초');
 
-$format_recur_lang['start']			= '매 %int% %freq% %for%';	// ie, 'Every 1 day until January 4' or 'Every 1 day for a count of 5'
-$format_recur_lang['until']			= '%date% 까지';				// ie, 'until January 4'
-$format_recur_lang['count']			= '%int% 회';		// ie, 'for 5 times'
+$format_recur_lang['start']			= '%int%%freq% 마다 %for%';	// ie, 'Every 1 day until January 4' or 'Every 1 day for a count of 5'
+$format_recur_lang['until']			= '%date%까지';				// ie, 'until January 4'
+$format_recur_lang['count']			= '%int%번';		// ie, 'for 5 times'
 
-$format_recur_lang['bymonth']		= '달에: %list%';			// ie, 'In months: January, February, March'
-$format_recur_lang['bymonthday']	= '날짜에: %list%';			// ie, 'On dates: 1, 2, 3, 4'
-$format_recur_lang['byday']			= '요일에: %list%';			// ie, 'On days: Mon, Tues, Wed, Thurs'
+$format_recur_lang['bymonth']		= '월：%list%';			// ie, 'In months: January, February, March'
+$format_recur_lang['bymonthday']	= '일：%list%';			// ie, 'On dates: 1, 2, 3, 4'
+$format_recur_lang['byday']			= '요일：%list%';			// ie, 'On days: Mon, Tues, Wed, Thurs'
+
+// ---------------------------------
+
 $daysofweek_lang			= array ('일요일','월요일','화요일','수요일','목요일','금요일','토요일');
 $daysofweekshort_lang		= array ('일','월','화','수','목','금','토');
 $daysofweekreallyshort_lang	= array ('일','월','화','수','목','금','토');
 $monthsofyear_lang			= array ('1월','2월','3월','4월','5월','6월','7월','8월','9월','10월','11월','12월');
-$monthsofyearshort_lang		= array ('1월','2월','3월','4월','5월','6월','7월','8월','9월','10월','11월','12월');
+$monthsofyearshort_lang		= $monthsofyear_lang;
 
 // For time formatting, check out: http://www.php.net/manual/en/function.date.php
 $timeFormat = 'g:i A';
 $timeFormat_small = 'g:i';
 
 // For date formatting, see note below
-$dateFormat_day = '%B %e일, %A';
-$dateFormat_week = '%B %e일';
-$dateFormat_week_list = '%b %e일, %a';
-$dateFormat_week_jump = '%b %e일';
-$dateFormat_month = '%Y년 %B';
-$dateFormat_month_list = '%B %e일, %A';
+$dateFormat_day = '%B%e일 %A';
+$dateFormat_week = '%B%e일';
+$dateFormat_week_list = '%b%e일（%a）';
+$dateFormat_week_jump = '%b%e일';
+$dateFormat_month = '%Y년%B';
+$dateFormat_month_list = '%B%e일 %A';
 $dateFormat_year = '%Y';
 
 /*
@@ -182,13 +195,15 @@ Notes about dateFormat_*
 */
 
 // Error messages - %s will be replaced with a variable
-$error_title_lang = '오류!';
-$error_window_lang = '에러가 발생했습니다!';
-$error_calendar_lang = ' 오류가 발생할 때 "%s" 달력이 처리중이었습니다.';
-$error_path_lang = '경로를 열지 못했습니다: "%s"';
-$error_back_lang = '돌아가시려면 "뒤로" 버튼을 사용하세요.';
-$error_remotecal_lang = '이 서버는 승인되지 않은 원격지 달력을 차단하고 있습니다.';
-$error_restrictedcal_lang = '이 서버에서 제한되고 있는 달력에 접근시도를 하셨습니다.';
-$error_invalidcal_lang = '올바르지 않은 달력파일입니다. 다른 달력으로 시도해보시기 바랍니다.';
+$lang['l_error_title'] 			= '에러!';
+$lang['l_error_window'] 		= '에러가 발생했습니다!';
+$lang['l_error_calendar'] 		= '에러가 발생했을 때, 「%s」달력을 처리하고 있었습니다.';
+$lang['l_error_path'] 			= '「%s」디렉토리를 열 수 없습니다.';
+$lang['l_error_back'] 			= '앞 페이지로 돌아가려면 「돌아가기」단추를 클릭하세요.';
+$lang['l_error_remotecal'] 		= '이 서버는 승인되지 않은 원격달력을 열지 않습니다.';
+$lang['l_error_restrictedcal'] 	= '이 서버로 제한된 달력에 접근하려고 했습니다.';
+$lang['l_error_invalidcal'] 	= '잘못된 파일입니다. 다른 달력으로 해 보세요.';
+$lang['l_error_cantopen'] 		= '달력을 열 수 없습니다.Unable to open calendar.';
+$lang['l_error_cache']			= '캐시디렉터리에 쓸 수 없습니다. 설정(config)을 확인하세요.';
 
 ?>
