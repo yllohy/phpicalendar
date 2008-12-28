c0739ccf3bda644d52394a3e4c768964dec0d40e
Jim Hu <jimhu@users.sourceforge.net>
2008-12-28 01:00:32 +0000

    language tweaks

--- languages/russian.inc.php
+++ languages/russian.inc.php
@@ -1,15 +1,15 @@
 <?php
 
-// English language include
-// For version 2.0 PHP iCalendar
+// Русский  подключаемый файл 
+// Для PHP iCalendar версии 2.2
 //
-// Translation by Chad Little (chad@chadsdomain.com)
+// Перевод выполнен Николаем Н. Комаровым (komarovNOSPAMpicture-group.ru)
 //
-// Submit new translations to phpicalendar@gmail.com
+// Новые переводы можно посылать по адресу phpicalendar@gmail.com
 
 $lang['l_day'] 				= 'День';
 $lang['l_week'] 			= 'Неделя';
-$lang['l_month'] 			= 'Месяц';
+$lang['l_month'] 			= 'Меcяц';
 $lang['l_year'] 			= 'Год';
 $lang['l_calendar'] 		= 'Календарь';
 $lang['l_next_day'] 		= 'Следующий день';
@@ -18,122 +18,122 @@ $lang['l_next_week'] 		= 'Следующая неделя';
 $lang['l_next_year'] 		= 'Следующий год';
 $lang['l_last_day'] 		= 'Предыдущий день';
 $lang['l_last_month'] 		= 'Предыдущий месяц';
-$lang['l_last_week'] 		= 'Предыдущий неделя';
+$lang['l_last_week'] 		= 'Предыдущая неделя';
 $lang['l_last_year'] 		= 'Предыдущий год';
 $lang['l_subscribe'] 		= 'Подписаться';
 $lang['l_download'] 		= 'Скачать';
-$lang['l_powered_by'] 		= 'Сделано ';
+$lang['l_powered_by'] 		= 'Работает на движке';
 $lang['l_event'] 			= 'Событие';
-$lang['l_event_start'] 		= 'Начало';
-$lang['l_event_end'] 		= 'Окончание';
-$lang['l_this_months'] 		= 'События месяца';
+$lang['l_event_start'] 		= 'Время начала';
+$lang['l_event_end'] 		= 'Время окончания';
+$lang['l_this_months'] 		= 'События этого месяца';
 $lang['l_date'] 			= 'Дата';
-$lang['l_summary'] 			= 'Итоги';
-$lang['l_all_day'] 			= 'События дня';
+$lang['l_summary'] 			= 'Кратко';
+$lang['l_all_day'] 			= 'Событие на весь день';
 $lang['l_notes'] 			= 'Заметки';
-$lang['l_this_years'] 		= 'События года';
+$lang['l_this_years'] 		= 'События этого года';
 $lang['l_today'] 			= 'Сегодня';
-$lang['l_this_week'] 		= 'Эта неделя';
-$lang['l_this_month'] 		= 'Этот месяц';
-$lang['l_jump'] 			= 'Перейти к:';
-$lang['l_tomorrows'] 		= 'События на завтра';
-$lang['l_goday'] 			= 'Перейти на сегодня';
-$lang['l_goweek'] 			= 'Перейти на эту неделю';
-$lang['l_gomonth'] 			= 'Перейти на этот месяц';
-$lang['l_goyear'] 			= 'Перейти на этот год';
-$lang['l_search'] 			= 'Поиск'; // the verb
+$lang['l_this_week'] 		= 'На этой неделе';
+$lang['l_this_month'] 		= 'В этом месяце';
+$lang['l_jump'] 			= 'Перейти к';
+$lang['l_tomorrows'] 		= 'Завтрашние события';
+$lang['l_goday'] 			= 'Сегодняшнему дню';
+$lang['l_goweek'] 			= 'Текущей неделе';
+$lang['l_gomonth'] 			= 'Текущему месяцу';
+$lang['l_goyear'] 			= 'Текущему году';
+$lang['l_search'] 			= 'Искать'; // the verb
 $lang['l_results'] 			= 'Результаты поиска';
-$lang['l_pick_multiple'] 	= 'Множественный выбор';
+$lang['l_pick_multiple'] 	= 'Выбор календарей';
 $lang['l_query'] 			= 'Запрос'; // will be followed by the search query
-$lang['l_no_results'] 		= 'Событий не найдено';
+$lang['l_no_results'] 		= 'События не найдены';
 $lang['l_goprint'] 			= 'Версия для печати';
 $lang['l_time'] 			= 'Время';
-$lang['l_summary'] 			= 'Итоги';
+$lang['l_summary'] 			= 'Кратко';
 $lang['l_description'] 		= 'Описание';
 $lang['l_this_site_is']		= 'Этот сайт ';
 $lang['l_no_events_day']	= 'Нет событий на сегодня.';
-$lang['l_no_events_week']	= 'Нет событий на эту неделю.';
-$lang['l_no_events_month']	= 'Нет событий на этот месяц.';
+$lang['l_no_events_week']	= 'Нет событий на этой неделе.';
+$lang['l_no_events_month']	= 'Нет событий в этом месяце.';
 $lang['l_rss_day_date']		= 'g:i A';  // Lists just the time
 $lang['l_rss_week_date	']	= '%b %e';  // Lists just the day
 $lang['l_rss_month_date	']	= '%b %e';  // Lists just the day
 $lang['l_rss_language']		= 'ru-ru';
-$lang['l_search_took']		= 'Затрачено на поиск %s секунд';
+$lang['l_search_took']		= 'Поиска занял %s секунд(ы)';
 $lang['l_recurring_event']	= 'Повторяющееся событие';
 $lang['l_exception']		= 'Исключение';
-$lang['l_no_query']			= 'Нет запроса';
-$lang['l_preferences']		= 'Настройки';
+$lang['l_no_query']			= 'Запрос не задан';
+$lang['l_preferences']		= 'Настройка';
 $lang['l_printer']			= 'Принтер';
-$lang['l_select_lang']		= 'Выбрать язык';
-$lang['l_select_cal']		= 'Выбрать календарь по умолчанию';
-$lang['l_select_view']		= 'Выбрать вид по умолчанию';
-$lang['l_select_time']		= 'Выбрать время начала';
-$lang['l_select_day']		= 'Выбрать начало недели по умолчанию';
-$lang['l_select_style']		= 'Выбрать стиль по умолчанию';
-$lang['l_set_prefs']		= 'Установить настройки';
-$lang['l_completed_date']	= 'Завершено в';
+$lang['l_select_lang']		= 'Язык:';
+$lang['l_select_cal']		= 'Календарь по умолчанию';
+$lang['l_select_view']		= 'Режим просмотра по умолчанию';
+$lang['l_select_time']		= 'Время начала по умолчанию';
+$lang['l_select_day']		= 'Первый день недели по умолчанию';
+$lang['l_select_style']		= 'Стиль по умолчанию';
+$lang['l_set_prefs']		= 'Cохранить настройки';
+$lang['l_completed_date']	= 'Завершено:';
 $lang['l_completed']		= 'Завершено';
 $lang['l_created']			= 'Создано';
-$lang['l_due']				= 'До';
+$lang['l_due']				= 'К дате';
 $lang['l_priority']			= 'Приоритет';
 $lang['l_priority_high']	= 'Высокий';
 $lang['l_priority_low']		= 'Низкий';
 $lang['l_priority_medium']	= 'Средний';
-$lang['l_priority_none']	= 'Нет';
-$lang['l_status']			= 'Статус';
+$lang['l_priority_none']	= '(нет)';
+$lang['l_status']			= 'Состояние';
 $lang['l_status_confirmed'] = 'ПОДТВЕРЖДЕНО' ; 
-$lang['l_status_cancelled'] = 'ОТМЕНЕНО'; 
-$lang['l_status_tentative'] = 'Под вопросом'; 
+$lang['l_status_cancelled'] = 'ОТВЕРГНУТО'; 
+$lang['l_status_tentative'] = 'В СОМНЕНИИ'; 
 $lang['l_todo']				= 'Задачи';
-$lang['l_unfinished']		= 'Незавершенная';
-$lang['l_prefs_set'] 		= 'Ваши установки применены.';
-$lang['l_prefs_unset'] 		= 'Ваши установки отменены. Изменения вступят в силу при следующей загрузке';
-$lang['l_unset_prefs'] 		= 'Отменить настройки';
-$lang['l_organizer']		= 'Организовать';
-$lang['l_attendee']			= 'Исполнено';
-$lang['l_location']			= 'Место';
-$lang['l_admin_header']		= 'Администрирование PHP iCalendar ';
-$lang['l_username']			= 'Логин';
+$lang['l_unfinished']		= 'Неокончено';
+$lang['l_prefs_set'] 		= 'Ваши настройки установлены.';
+$lang['l_prefs_unset'] 		= 'Настройки сброшены. Изменения вступят в силу при следующей загрузке страницы.';
+$lang['l_unset_prefs'] 		= 'Сбросить настройки';
+$lang['l_organizer']		= 'Организатор';
+$lang['l_attendee']			= 'Участник';
+$lang['l_location']			= 'Место проведения';
+$lang['l_admin_header']		= 'Администрирование PHP iCalendar';
+$lang['l_username']			= 'Имя пользователя';
 $lang['l_password']			= 'Пароль';
 $lang['l_login']			= 'Логин';
-$lang['l_invalid_login']	= 'Неправильный логин или пароль.';
+$lang['l_invalid_login']	= 'Неправильные имя пользователя или пароль.';
 $lang['l_addupdate_cal']	= 'Добавить или обновить календарь';
-$lang['l_addupdate_desc']	= 'Добавить календарь через загрузку нового файла. Обновить календарь через перезапись файла с таким же именем.';
+$lang['l_addupdate_desc']	= 'Для добавления календаря загрузите на сервер файл. Для обновления файла закачайте файл с тем же именем.';
 $lang['l_delete_cal']		= 'Удалить календарь';
-$lang['l_logout']			= 'Выход';
+$lang['l_logout']			= 'Выйти из системы';
 $lang['l_cal_file']			= 'Файл календаря';
-$lang['l_php_error']		= 'PHP Ошибка';
-$upload_error_gen_lang		= 'Проблема с вашей загрузкой.';
-$upload_error_lang[0]		= 'Проблема с вашей загрузкой.';
-$upload_error_lang[1]		= 'Файл слишком большой.';
-$upload_error_lang[2]		= 'Файл слишком большой.';
-$upload_error_lang[3]		= 'Файл частично загружен.';
-$upload_error_lang[4]		= 'Вы должны выбрать файл для загрузки.';
-$lang['l_upload_error_type'] = 'Только файлы с расширением .ics files могут быть загружены.';
-$lang['l_copy_error']			= 'Ошибка при копировании файла';
-$lang['l_delete_error']			= 'Ошибка при удалении файла';
-$lang['l_delete_success']		= 'файл удален.';
-$lang['l_action_success']		= 'Действие успешно завершено';
-$lang['l_submit']				= 'Подтвердить';
+$lang['l_php_error']		= 'Ошибка PHP';
+$upload_error_gen_lang		= 'При закачивании возникла проблема.';
+$upload_error_lang[0]		= 'При закачивании возникла проблема.';
+$upload_error_lang[1]		= 'Файл, который Вы хотите закачать, слишком велик.';
+$upload_error_lang[2]		= ' Файл, который Вы хотите закачать, слишком велик.';
+$upload_error_lang[3]		= 'Ваш файл закачался лишь частично.';
+$upload_error_lang[4]		= 'Вы должны выбрать файл календаря для закачивания.';
+$lang['l_upload_error_type'] = 'Закачивать можно только файлы с расширением .ics.';
+$lang['l_copy_error']			= 'Не удалось скопировать файл';
+$lang['l_delete_error']			= 'Не удалось удалить файл';
+$lang['l_delete_success']		= 'успешно удален.';
+$lang['l_action_success']		= 'Действие выполнено успешно.';
+$lang['l_submit']				= 'Послать';
 $lang['l_delete']				= 'Удалить';
-$all_cal_comb_lang			= 'Все вместе';
+$all_cal_comb_lang			= 'Все одновременно';
 
 // New for 2.0
-$lang['l_legend']			= 'Легенда';
-$lang['l_admin_subhead']	= 'Управлять вашим календарем с этой страницы';
-$lang['l_prefs_subhead']	= 'Установить cookie для быстрой аутентификации на этом сайте';
-$lang['l_rss_info']			= 'RSS информация';
-$lang['l_rss_subhead']		= 'Базовые RSS доступны для каждого календаря';
-$lang['l_rss_notenabled']	= 'RSS отключено на этом сайте';
+$lang['l_legend']			= 'Легенда (цвета)';
+$lang['l_admin_subhead']	= 'На этой странице можно управлять календарями';
+$lang['l_prefs_subhead']	= 'Устанавливает cookie-файлы для посетителей';
+$lang['l_rss_info']			= 'RSS-информация';
+$lang['l_rss_subhead']		= 'Для каждого календаря доступны простые RSS-потоки';
+$lang['l_rss_notenabled']	= 'На этом сайте RSS не включен';
 
 // - navigation
 $lang['l_back'] 		= 'Назад';
-$lang['l_next'] 		= 'Вперед';
-$lang['l_prev'] 		= 'Предыдущая';
-$lang['l_day_view'] 	= 'День';
-$lang['l_week_view'] 	= 'Неделя';
-$lang['l_month_view'] 	= 'Месяц';
-$lang['l_year_view'] 	= 'Год';
+$lang['l_next'] 		= 'След.';
+$lang['l_prev'] 		= 'Пред.';
+$lang['l_day_view'] 	= 'Просмотр дня';
+$lang['l_week_view'] 	= 'Просмотр недели';
+$lang['l_month_view'] 	= 'Просмотр месяца';
+$lang['l_year_view'] 	= 'Просмотр года';
 
 // ---------------------------------
 
@@ -143,27 +143,27 @@ $format_recur_lang['delimiter']	= ', ';								// ie, 'one, two, three'
 
 $format_recur_lang['yearly']		= array('год','годы');		// for these, put singular
 $format_recur_lang['monthly']		= array('месяц','месяцы');		// and plural forms
-$format_recur_lang['weekly']		= array('неделя','недели');		// these will be %freq%
-$format_recur_lang['daily']			= array('день','дни');			// in the replacement below
-$format_recur_lang['hourly']		= array('час','часы');
-$format_recur_lang['minutely']		= array('минута','минуты');
-$format_recur_lang['secondly']		= array('секунда','секунды');
+$format_recur_lang['weekly']		= array('неделя','недель');		// these will be %freq%
+$format_recur_lang['daily']			= array('день','дней');			// in the replacement below
+$format_recur_lang['hourly']		= array('час','часов');
+$format_recur_lang['minutely']		= array('минута','минут');
+$format_recur_lang['secondly']		= array('секунда','секунд');
 
 $format_recur_lang['start']			= 'Каждые %int% %freq% %for%';	// ie, 'Every 1 day until January 4' or 'Every 1 day for a count of 5'
 $format_recur_lang['until']			= 'до %date%';				// ie, 'until January 4'
-$format_recur_lang['count']			= 'счетчик каждые %int%';		// ie, 'for 5 times'
+$format_recur_lang['count']			= '%int% раз';		// ie, 'for 5 times'
 
-$format_recur_lang['bymonth']		= 'В месяце: %list%';			// ie, 'In months: January, February, March'
-$format_recur_lang['bymonthday']	= 'В датах: %list%';			// ie, 'On dates: 1, 2, 3, 4'
-$format_recur_lang['byday']			= 'В днях: %list%';			// ie, 'On days: Mon, Tues, Wed, Thurs'
+$format_recur_lang['bymonth']		= 'По месяцам: %list%';			// ie, 'In months: January, February, March'
+$format_recur_lang['bymonthday']	= 'По числам: %list%';			// ie, 'On dates: 1, 2, 3, 4'
+$format_recur_lang['byday']			= 'По дням: %list%';			// ie, 'On days: Mon, Tues, Wed, Thurs'
 
 // ---------------------------------
 
-$$daysofweek_lang			= array ('Воскресенье','Понедельник','Вторник','Среда','Четверг','Пятница','Суббота');
-$daysofweekshort_lang		= array ('Вск','Пнд','Втр','Срд','Чтв','Птн','Суб');
-$daysofweekreallyshort_lang	= array ('В','П','В','С','Ч','Т','С');
+$daysofweek_lang			= array ('Воскресенье','Понедельник','Вторник','Среда','Четверг','Пятница','Суббота');
+$daysofweekshort_lang		= array ('Вск','Пнд','Втр','Срд','Чтв','Птн','Сбт');
+$daysofweekreallyshort_lang	= array ('В','П','В','С','Ч','П','C');
 $monthsofyear_lang			= array ('Январь','Февраль','Март','Апрель','Май','Июнь','Июль','Август','Сентябрь','Октябрь','Ноябрь','Декабрь');
-$monthsofyearshort_lang		= array ('Янв','Фев','Мар','Апр','Май','Июн','Июл','Авг','Сен','Окт','Ноя','Дек');
+$monthsofyearshort_lang		= array ('Янв','Фев','Мар','Апр','Май','Инь',' Иль','Авг','Сен','Окт','Ноя','Дек');
 
 // For time formatting, check out: http://www.php.net/manual/en/function.date.php
 $timeFormat = 'g:i A';
@@ -196,15 +196,15 @@ Notes about dateFormat_*
 */
 
 // Error messages - %s will be replaced with a variable
-$lang['l_error_title'] 			= 'ошибка!';
-$lang['l_error_window'] 		= 'ошибка !';
-$lang['l_error_calendar'] 		= 'Календарь "%s" был обработан с ошибкой.';
-$lang['l_error_path'] 			= 'Невозможно открыть путь: "%s"';
-$lang['l_error_back'] 			= 'Нажмите клавишу "Назад" для возврата.';
-$lang['l_error_remotecal'] 		= 'Этот сервер блокирует удаленные календари.';
-$lang['l_error_restrictedcal'] 	= 'Вы пытаетесь получить доступ к ограниченному календарю.';
-$lang['l_error_invalidcal'] 	= 'Неправильный формат файла календаря.';
-$lang['l_error_cantopen'] 		= 'Невозможно открыть календарь.';
-$lang['l_error_cache']			= 'Нет возможности записать в кэш директорию. Проверьте свою конфигурацию';
+$lang['l_error_title'] 			= 'Ошибка!';
+$lang['l_error_window'] 		= 'Произошла ошибка!';
+$lang['l_error_calendar'] 		= 'Когда произошла ошибка, обрабатывался каледарь "%s".';
+$lang['l_error_path'] 			= 'Не удается открыть путь: "%s"';
+$lang['l_error_back'] 			= 'Для возврата используйте кнопку "Назад".';
+$lang['l_error_remotecal'] 		= 'Этот сервер блокирует удаленно расположенные календари, которые не прошли проверку.';
+$lang['l_error_restrictedcal'] 	= 'Вы попытались обратиться к календарю, доступ к которому ограничен.';
+$lang['l_error_invalidcal'] 	= 'Некорректный/поврежденный файл календаря. Попробуйте другой календарь, пожалуйста.';
+$lang['l_error_cantopen'] 		= 'Не удается открыть календарь.';
+$lang['l_error_cache']			= 'Неудается записать данные в каталог с кэшем. Проверьте конфигурацию., пожалуйста.';
 
 ?>
