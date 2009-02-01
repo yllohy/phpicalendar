e93f8f6c8b6dc849506e2a50aa86fa54b77576e8
Jim Hu <jimhu@users.sourceforge.net>
2009-02-01 22:45:11 +0000

    improvements in rss

--- rss/rss1.0.php
+++ rss/rss1.0.php
@@ -9,7 +9,7 @@ J. Hu 12/10/2008
 define('BASE', '../');
 require(BASE.'rss/rss_common.php');
 function rss_top(){
-	global $cal_displayname, $theview, $default_path, $cpath, $lang, $rss_link, $rss_language;
+	global $cal_displayname, $theview, $phpiCal_config, $cpath, $lang, $rss_link, $rss_language;
 	$rss = 	"<?xml version=\"1.0\" encoding=\"UTF-8\"?>"."\n";
 	
 	/* Use 1.0 and strip encoding, use rss_language */
