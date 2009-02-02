ef71959d6f605170a7fdd6ca0d799197054e6e40
Jim Hu <jimhu@users.sourceforge.net>
2009-02-02 04:11:59 +0000

    bump version

--- default_config.php
+++ default_config.php
@@ -2,8 +2,8 @@
 class Configs{
 	private static $instance;
 	private function __construct(){
-		$this->phpicalendar_version = '2.31';
-	// Configuration file for PHP iCalendar 2.31
+		$this->phpicalendar_version = '2.32alpha';
+	// Configuration file for PHP iCalendar 2.32
 	//
 	// To set values, change the text between the single quotes
 	// Follow instructions to the right for detailed information
